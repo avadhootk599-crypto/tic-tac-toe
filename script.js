47e59130383914ca76f7ae1b536e9709b30af5aa
avadhootk599-crypto <avadhootk599@gmail.com>
2026-07-24 13:09:06 +0530

    Implement game controller logic with player turns and win conditions

--- script.js
+++ script.js
@@ -30,3 +30,82 @@ const Player = (name, mark) => {
   };
   return { getmark, getscore, incrementScore };
 };
+
+const gamecontroller = (function () {
+  const player = [Player("Player1", "X"), Player("Player2", "O")];
+  let activePlayerIndex = 0;
+  let gameOver = false;
+  let winner = null;
+
+  const winningCombos = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  const getActivePlayer = () => player[activePlayerIndex];
+  const getPlayers = () => players;
+  const isGameOver = () => gameOver;
+  const getWinner = () => winner;
+
+  const switchTurn = () => {
+    activePlayerIndex = activePlayerIndex === 0 ? 1 : 0;
+  };
+
+  const checkWinner = () => {
+    const board = GameBoard.getboard();
+    for ([a, b, c] of winningCombos) {
+      if (board[a] && board[a] === board[b] && board[a] === board[c]) {
+        return board[a];
+      }
+    }
+  };
+
+  const playRound = (index) => {
+    if (gameOver) return false;
+
+    const placed = GameBoard.placemark(index, getActivePlayer(), getmark());
+    if (!placed) return false;
+
+    const winningmark = checkWinner();
+    if (winningmark) {
+      gameOver = true;
+      winner = player.find((p) => p.getmark() === winningmark);
+      winner.incrementScore();
+    } else if (GameBoard.isfull()) {
+      gameOver = true;
+      winner = "tie";
+    } else {
+      switchTurn();
+    }
+
+    return true;
+  };
+
+  const playAgain = () => {
+    activePlayerIndex = 0;
+    gameOver = false;
+    winner = null;
+  };
+
+  const reset = () => {
+    player[0] = player(names[0], "X");
+    player[1] = player(names[0], "O");
+    playAgain();
+  };
+
+  return {
+    getActivePlayer,
+    getPlayers,
+    isGameOver,
+    getWinner,
+    playRound,
+    playAgain,
+    reset,
+  };
+})();
